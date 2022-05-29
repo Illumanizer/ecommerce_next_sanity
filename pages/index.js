ec82ebe3f6fd79c9d270f54268bf2e7c75685647
Illumanizer <74318570+Illumanizer@users.noreply.github.com>
2022-05-29 22:48:17 +0530

    best

--- pages/index.js
+++ pages/index.js
@@ -7,7 +7,7 @@ const Home = ({products,bannerData}) => {
     <HeroBanner heroBanner={bannerData.length && bannerData[0]}/>
     {console.log(bannerData)}
     <div className='products-heading'>
-      <h2>Beset Selling Products</h2>
+      <h2>Best Selling Products</h2>
       <p>Speakers of many Variation</p>
     </div>
 
